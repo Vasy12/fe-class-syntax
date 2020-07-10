0f1460fd9e7ed0b243fbfc9b71d8d7d99be8331d
User Name <artur.it2914@gmail.com>
2020-07-10 20:22:02 +0300

    Add getters setters examples

--- new-syntax.js
+++ new-syntax.js
@@ -8,22 +8,41 @@ class User {
    * @param {number} age
    */
   constructor(name, surname, age) {
-    this.name = name;
-    this.surname = surname;
+    this._name = name;
+    this._surname = surname;
+
     this.age = age;
+
   }
 
-  isAdult() {
-    return this.age >= 18;
+  set age(v) {
+    if (typeof v !== 'number') {
+      throw new TypeError( 'param age must be a number' );
+    }
+
+    if (v < 0 || !Number.isInteger( v )) {
+      throw new RangeError( 'age value must be positive integer number' );
+    }
+
+    this._age = v;
   }
 
-  getFullName() {
-    return `${this.name} ${this.surname}`;
+  get age() {
+    return this._age;
+  }
+
+  /**
+   *
+   * @returns {boolean} - user adults status
+   */
+  get isAdult() {
+    return this._age >= 18;
+  }
+
+  get fullName() {
+    return `${this._name} ${this._surname}`;
   }
 }
-
-const user1 = new User( 'Sasha', 'Ivanov', 24 );
-const user2 = new User( 'Dasha', 'Ivanova', 17 );
 
 /**
  * Реализуйте класс Worker (Работник),
@@ -49,10 +68,75 @@ class Worker {
 
   }
 
-  getSalary() {
+  set days(v) {
+
+    if (typeof v !== 'number') {
+      throw new TypeError();
+    }
+    if (v < 0 || v > 31 || !Number.isInteger( v )) {
+      throw new RangeError();
+    }
+
+    this._days = v;
+
+  }
+
+  get days() {
+    return this._days;
+  }
+
+  set rate(v) {
+
+    if (typeof v !== 'number') {
+      throw new TypeError();
+    }
+
+    if (v < 0) {
+      throw new RangeError();
+    }
+
+    this._rate = v;
+  }
+
+  get rate() {
+    return this._rate;
+  }
+
+  get salary() {
     return this.days * this.rate;
   }
 
 }
 
+class Fuel {
+  constructor(volume, density) {
+    this.volume = volume;
+    this.density = density;
+  }
 
+  get weight() {
+    return this.density * this.volume;
+  }
+}
+
+class Car {
+  /**
+   *
+   * @param {number} weight
+   * @param {Fuel} fuel
+   */
+  constructor(weight, fuel) {
+    this.ownWeight = weight;
+    this.fuel = fuel;
+  }
+
+  get curbWeight() {
+    const fuelWeight = this.fuel.weight;
+    return this.ownWeight + fuelWeight;
+  }
+
+}
+
+const car1 = new Car( 1200, new Fuel( 50, 0.79 ) );
+
+const car1CurbWeight = car1.curbWeight;
