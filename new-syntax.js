b1477bb2c4d48a2892ee8296961e681f49c36ade
User Name <artur.it2914@gmail.com>
2020-07-13 19:11:31 +0300

    Add class inherit example

--- new-syntax.js
+++ new-syntax.js
@@ -1,41 +1,76 @@
 'use strict';
 
 class User {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+  constructor(name, surname, age, email, isBanned = false) {
+    this._name = name;
+    this._surname = surname;
+    this._age = age;
+    this._email = email;
+    this._isBanned = isBanned;
   }
 
-  get isAdult() {
-    return this.age >= 18;
-  }
-
-  static isUser(obj) {
-
-    return obj instanceof User;
-  }
-
-  static isAdult(obj) {
-    if (!User.isUser( obj )) {
+  set isBanned(v) {
+    if (typeof v !== 'boolean') {
       throw new TypeError();
     }
-    return obj.isAdult;
+    this._isBanned = v;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  getFullName() {
+    return `${this._name} ${this._surname}`;
+  }
+
+  isAdult() {
+    return this._age > 17;
   }
 
 }
 
-const user1 = new User( 'Test', 18 );
+class Admin extends User {
 
-User.isUser( user1 );
-
-const obj1 = {
-  isF() {
-
+  constructor(name, surname, age, email) {
+    super( name, surname, age, email, false );
   }
-};
 
-const obj2 = {
-  isF() {
-
+  reBan(user) {
+    user.isBanned = false;
   }
-};
+
+  ban(user) {
+    user.isBanned = true;
+  }
+
+}
+
+class Moderator extends User {
+
+  constructor(name, surname, age, email, permission) {
+    super( name, surname, age, email );
+
+    this._permission = permission;
+  }
+
+  sendMessage(user, message) {
+
+    const userEmail = user.email;
+    /*
+     * send message code
+     * */
+  }
+
+}
+
+const user1 = new User( 'Nasme', 'Suisdf', 14, 'test@gmail.com' );
+
+const moder1 = new Moderator( 'Moder', 'Moderovich', 24, 'moder@email.com', {} );
+
+user1.isAdult();
+user1.getFullName();
+
+moder1.isAdult();
+moder1.getFullName();
+moder1.sendMessage( user1, { body: 'Hello' } );
