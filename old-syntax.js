a1f32476bbcf28b8df216a6fa6627c8b99a47b61
User Name <artur.it2914@gmail.com>
2020-07-13 18:42:41 +0300

    Add RangeValidator solution

--- old-syntax.js
+++ old-syntax.js
@@ -1,20 +1,53 @@
 'use strict';
 
-function User(name, surname) {
+class RangeValidator {
 
-  this.firstName = name;
-  this.lastName = surname;
+  constructor(from, to) {
+    if (typeof from !== 'number' || typeof to !== 'number') {
+      throw new TypeError();
+    }
+    if (from > to) {
+      throw new RangeError();
+    }
+
+    this._from = from;
+    this._to = to;
+  }
+
+  set from(v) {
+    if (typeof v !== 'number') {
+      throw new TypeError();
+    }
+    if (v > this._to) {
+      throw new RangeError();
+    }
+
+    this._from = v;
+  }
+
+  get range() {
+    return [this._from, this._to];
+  }
+
+  validate(n) {
+
+    if (typeof n !== 'number') {
+      throw new TypeError();
+    }
+
+    if (n >= this._from && n <= this._to) {
+      return n;
+    }
+
+    throw new RangeError();
+
+  }
+
+  isValid(n) {
+    if (typeof n !== 'number') {
+      return false;
+    }
+    return Boolean( n >= this._from && n <= this._to );
+  }
 
 }
-
-const userProto = new User();
-
-userProto.getFullName = function getFullName() {
-  return `${this.firstName} ${this.lastName}`;
-};
-
-User.prototype = userProto;
-
-const user1 = new User( 'Petryk', 'Piatochkin' );
-
-user1.getFullName();
