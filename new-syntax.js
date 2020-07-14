fb5ac9dd5732353ada4cc34ee9e18e6f01af10dc
User Name <artur.it2914@gmail.com>
2020-07-14 20:23:36 +0300

    Add Symbol.iterator example

--- new-syntax.js
+++ new-syntax.js
@@ -1,87 +1,170 @@
 'use strict';
 
-function MyArray() {
-  this.length = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    this[this.length++] = arguments[i];
+/**
+ *
+ * number
+ * boolean
+ * null
+ * undefined
+ *
+ * symbol
+ * string
+ *
+ * bigint
+ *
+ * Object
+ *
+ */
+
+class MyArrayIterator {
+  /**
+   *
+   * @param {MyArray} myArray
+   */
+  constructor(myArray) {
+    this._myArray = myArray;
+    this._currentIndex = 0;
+  }
+
+  next() {
+    if (this._currentIndex >= this._myArray.length) {
+      return {
+        value: undefined,
+        done: true,
+      };
+    }
+    return {
+      value: this._myArray[this._currentIndex++],
+      done: false
+    };
   }
 }
 
-MyArray.isMyArray = function (obj) {
-  return obj instanceof this;
-};
-
-MyArray.prototype = {
-  length: 0,
-  push() {
-    for (let i = 0; i < arguments.length; i++) {
-      this[this.length++] = arguments[i];
+class MyArray {
+  constructor(...args) {
+    this.length = 0;
+    for (const item of args) {
+      this[this.length++] = item;
     }
-    return this.length;
-  },
-  concat() {
-    const result = new MyArray();
+  }
 
-    for (let i = 0; i < this.length; i++) {
-      result.push( this[i] );
+  [Symbol.iterator]() {
+    return new MyArrayIterator( this );
+  }
+
+}
+
+const test = new MyArray( 1, 2, 3, 4, 5, 6, 7, 8, 9 );
+
+for (const item of test) {
+  console.log( item );
+}
+
+const arr = [...test];
+
+class ListItem {
+  /**
+   *
+   * @param {*} value
+   * @param {ListItem | null} next
+   * @param {ListItem | null} prev
+   */
+  constructor(value, next = null, prev = null) {
+
+    this._value = value;
+
+    this._next = next;
+    this._prev = prev;
+
+  }
+
+  set next(node) {
+    if (node instanceof ListItem) {
+      this._next = node;
+    }
+  }
+
+  get next() {
+    return this._next;
+  }
+
+  set prev(node) {
+    if (node instanceof ListItem) {
+      this._prev = node;
+    }
+  }
+
+  get prev() {
+    return this._prev;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+}
+
+class LinkedListIterator {
+  constructor(list) {
+    this._list = list;
+    this._currentNode = null;
+  }
+
+  next() {
+    this._currentNode = this._currentNode
+                        ? this._currentNode.next
+                        : this._list.head;
+
+    return {
+      value: this._currentNode
+             ? this._currentNode.value
+             : undefined,
+      done: !this._currentNode,
+    };
+  }
+}
+
+class LinkedList {
+  constructor(...args) {
+    this.length = 0;
+
+    this._head = null;
+    this._tail = null;
+
+    for (const item of args) {
+      this.push( item );
     }
 
-    for (let i = 0; i < arguments.length; i++) {
-      if (MyArray.isMyArray( arguments[i] )) {
-        for (let j = 0; j < arguments[i].length; j++) {
-          result.push( arguments[i][j] );
-        }
-        continue;
-      }
-      result.push( arguments[i] );
+  }
+
+  get head() {
+    return this._head;
+  }
+
+  push(v) {
+    const newNode = new ListItem( v );
+
+    if (this.length === 0) {
+      this._head = newNode;
+      this._tail = newNode;
+    } else {
+
+      this._tail.next = newNode;
+      newNode.prev = this._tail;
+
+      this._tail = newNode;
     }
 
-    return result;
-  },
-  flat(dept = 1) {
-    let result = new MyArray();
-    for (let i = 0; i < this.length; i++) {
-      if (MyArray.isMyArray( this[i] ) && dept > 0) {
-        const tmp = this[i].flat( dept - 1 );
-        result = result.concat( tmp );
-      } else if (this[i] !== undefined) {
-        result.push( this[i] );
-      }
-    }
-    return result;
-  },
-};
+    return ++this.length;
 
-const arr = new MyArray(
-  new MyArray(
-    1,
-    undefined,
-    1,
-  ),
-  0,
-  undefined,
-  0,
-  0,
-  new MyArray(
-    1,
-    undefined,
-    undefined,
-    1,
-    new MyArray(
-      2,
-      new MyArray(
-        undefined,
-        undefined,
-        3,
-        3,
-        3,
-      ),
-      undefined,
-      2,
-      2,
-    ),
-    1,
-  )
-);
+  }
 
-const flattedArr = arr.flat();
+  [Symbol.iterator]() {
+    return new LinkedListIterator( this );
+  }
+
+}
+
+const list = new LinkedList( 1, 2, 3, 4, 5, 6, 7, 8, 9 );
+
+const arr2 = [...list];
--- ui.js
+++ ui.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+
+
 /**
  * Button
  */
